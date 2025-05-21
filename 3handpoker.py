1cead69b3d2209791ecbef0898948ff580f4a4a9
Nathan Zhang <nathan.zhang@roxburylatin.org>
2025-05-20 22:59:54 -0400

    attempt to add bot

--- 3handpoker.py
+++ 3handpoker.py
@@ -18,7 +18,7 @@ rank_values = {r: i for i, r in enumerate(ranks, 2)}
 FONT = pygame.font.SysFont(None, 32)
 BIG_FONT = pygame.font.SysFont(None, 48, bold=True)
 
-small_blind = 500
+small_blind = 1000
 big_blind = 1000
 bet_made = True
 player_is_bb = True
@@ -31,9 +31,9 @@ hands = []
 deck = []
 button_locked_until = 0
 if player_is_bb:
-    bet_history = [(1, 0.5), (0, 1)]
+    bet_history = [(1, 1), (0, 1)]
 else:
-    bet_history = [(0, 0.5), (1, 1)]
+    bet_history = [(0, 1), (1, 1)]
 show_cards = False
 pre_flop = True
 
@@ -59,9 +59,9 @@ def reset_round():
     deck = get_shuffled_deck()
     hands = [deck[0:3], deck[3:6]]
     if player_is_bb:
-        bet_history = [(1, 0.5), (0, 1)]
+        bet_history = [(1, 1), (0, 1)]
     else:
-        bet_history = [(0, 0.5), (1, 1)]
+        bet_history = [(0, 1), (1, 1)]
     show_cards = False
     pre_flop = True
 
@@ -199,10 +199,10 @@ def handle_action(action, bet_amount, player):
             bot_stacks -= bet_history[-1][1] * big_blind
             show_cards = True
             draw_hand(hands[1], 175, 50)
-            if determine_winner() == "player":
+            if determine_winner(hands[0], hands[1]) == "player":
                 player_stacks += pot_size
                 print("Player Wins!")
-            elif determine_winner() == "bot":
+            elif determine_winner(hands[0], hands[1]) == "bot":
                 bot_stacks += pot_size
                 print("Bot Wins!")
             else:
@@ -215,10 +215,10 @@ def handle_action(action, bet_amount, player):
         if (player == 0 and player_is_bb) or (player == 1 and not player_is_bb):
             show_cards = True
             draw_hand(hands[1], 175, 50)
-            if determine_winner() == "player":
+            if determine_winner(hands[0], hands[1]) == "player":
                 player_stacks += pot_size
                 print("Player Wins!")
-            elif determine_winner() == "bot":
+            elif determine_winner(hands[0], hands[1]) == "bot":
                 bot_stacks += pot_size
                 print("Bot Wins!")
             else:
@@ -230,10 +230,10 @@ def handle_action(action, bet_amount, player):
             pot_size += bet_history[-1][1] * 2 * big_blind
             player_stacks -= bet_history[-1][1] * big_blind
             bot_stacks -= bet_history[-1][1] * big_blind
-            if determine_winner() == "player":
+            if determine_winner(hands[0], hands[1]) == "player":
                 player_stacks += pot_size
                 print("Player Wins!")
-            elif determine_winner() == "bot":
+            elif determine_winner(hands[0], hands[1]) == "bot":
                 bot_stacks += pot_size
                 print("Bot Wins!")
             else:
@@ -265,9 +265,9 @@ def evaluate_hand(hand):
     else:
         return (0, values)
 
-def determine_winner():
-    player_best = evaluate_hand(list(hands[0]))
-    bot_best = evaluate_hand(list(hands[1]))
+def determine_winner(hand1, hand2):
+    player_best = evaluate_hand(list(hand1))
+    bot_best = evaluate_hand(list(hand2))
     if player_best > bot_best:
         return "player"
     elif bot_best > player_best:
@@ -302,6 +302,248 @@ def draw_player_info():
         if player == 0 and player_bet < bet_amount:
             player_bet = bet_amount
     screen.blit(FONT.render(format_number(player_bet*big_blind), True, (255, 255, 255)), (45, 480))
+
+
+
+
+# import numpy as np
+# import random
+# from collections import defaultdict
+
+# class Node:
+# 	def __init__(self, bet_options):
+# 		self.num_actions = len(bet_options)
+# 		self.regret_sum = defaultdict(int)
+# 		self.strategy = defaultdict(int)
+# 		self.strategy_sum = defaultdict(int)
+# 		self.bet_options = bet_options
+
+# 	def get_strategy(self):
+# 		normalizing_sum = 0
+# 		for a in self.bet_options:
+# 			if self.regret_sum[a] > 0:
+# 				self.strategy[a] = self.regret_sum[a]
+# 			else:
+# 				self.strategy[a] = 0
+# 			normalizing_sum += self.strategy[a]
+
+# 		for a in self.bet_options:
+# 			if normalizing_sum > 0:
+# 				self.strategy[a] /= normalizing_sum
+# 			else:
+# 				self.strategy[a] = 1.0/self.num_actions
+
+# 		return self.strategy
+
+# 	def get_average_strategy(self):
+# 		avg_strategy = defaultdict(int)
+# 		normalizing_sum = 0
+		
+# 		for a in self.bet_options:
+# 			normalizing_sum += self.strategy_sum[a]
+# 		for a in self.bet_options:
+# 			if normalizing_sum > 0:
+# 				avg_strategy[a] = self.strategy_sum[a] / normalizing_sum
+# 			else:
+# 				avg_strategy[a] = 1.0 / self.num_actions
+		
+# 		return avg_strategy
+
+# class LeducCFR:
+# 	def __init__(self, iterations, decksize, starting_stack):
+# 		#self.nbets = 2
+# 		self.iterations = iterations
+# 		self.decksize = decksize
+# 		self.bet_options = starting_stack
+# 		self.cards = sorted(np.concatenate((np.arange(decksize),np.arange(decksize))))
+# 		self.nodes = {}
+
+# 	def cfr_iterations_external(self):
+# 		util = np.zeros(2)
+# 		for t in range(1, self.iterations + 1): 
+# 			for i in range(2):
+# 					random.shuffle(self.cards)
+# 					util[i] += self.external_cfr(self.cards[:3], [[], []], 0, 2, 0, i, t)
+# 		print('Average game value: {}'.format(util[0]/(self.iterations)))
+		
+# 		with open('leducnlstrat.txt', 'w+') as f:
+# 			for i in sorted(self.nodes):
+# 				f.write('{}, {}\n'.format(i, self.nodes[i].get_average_strategy()))
+# 				print(i, self.nodes[i].get_average_strategy())
+
+# 	def winning_hand(self, cards):
+# 		if cards[0] == cards[2]:
+# 			return 0
+# 		elif cards[1] == cards[2]:
+# 			return 1
+# 		elif cards[0] > cards[1]:
+# 			return 0
+# 		elif cards[1] > cards[0]:
+# 			return 1
+# 		elif cards[1] == cards[0]:
+# 			return -1
+
+# 	def valid_bets(self, history, rd, acting_player):
+# 		if acting_player == 0:
+# 			acting_stack = int(19 - (np.sum(history[0][0::2]) + np.sum(history[1][0::2])))
+# 		elif acting_player == 1:
+# 			acting_stack = int(19 - (np.sum(history[0][1::2]) + np.sum(history[1][1::2])))
+
+
+# 		# print('VALID BETS CHECK HISTORY', history)
+# 		# print('VALID BETS CHECK ROUND', rd)
+# 		# print('VALID BETS CHECK ACTING STACK', acting_stack)
+# 		curr_history = history[rd]
+
+
+# 		if len(history[rd]) == 0:
+# 			# print('CASE LEN 0', [*np.arange(acting_stack+1)])
+# 			return [*np.arange(acting_stack+1)]
+
+# 		elif len(history[rd]) == 1:
+# 			min_raise = curr_history[0]*2
+# 			call_amount = curr_history[0]
+# 			if min_raise > acting_stack:
+# 				if history[rd] == [acting_stack]:
+# 					# print('CASE LEN 1', [0, acting_stack])
+# 					return [0, acting_stack]
+# 				else:
+# 					# print('CASE LEN 1', [0, call_amount, acting_stack])
+# 					return [0, call_amount, acting_stack]
+# 			else:
+# 				if history[rd] == [0]:
+# 					# print('CASE LEN 1', [*np.arange(min_raise, acting_stack+1)])
+# 					return [*np.arange(min_raise, acting_stack+1)]
+# 				else:
+# 					# print('CASE LEN 1', [0, call_amount, *np.arange(min_raise, acting_stack+1)])
+# 					return [0, call_amount, *np.arange(min_raise, acting_stack+1)]
+
+# 		elif len(history[rd]) == 2:
+# 			min_raise = 2*(curr_history[1] - curr_history[0])
+# 			call_amount = curr_history[1] - curr_history[0]
+# 			if min_raise > acting_stack:
+# 				if call_amount == acting_stack:
+# 					# print('CASE LEN 2', [0, acting_stack])
+# 					return [0, acting_stack]
+# 				else:
+# 					# print('CASE LEN 2', [0, call_amount, acting_stack])
+# 					return [0, call_amount, acting_stack]
+# 			else:
+# 				# print('CASE LEN 2', [0, call_amount, *np.arange(min_raise, acting_stack+1)])
+# 				return [0, call_amount, *np.arange(min_raise, acting_stack+1)]
+
+# 		elif len(history[rd]) == 3:
+# 			call_amount = np.abs(curr_history[1] - curr_history[2] - curr_history[0])
+# 			# print('CASE LEN 3', [0, call_amount])
+# 			return [0, call_amount] #final bet (4 maximum per rd)
+
+# 	def external_cfr(self, cards, history, rd, pot, nodes_touched, traversing_player, t):
+# 		if t % 1000 == 0 and t>0:
+# 			print('THIS IS ITERATION', t)
+# 		plays = len(history[rd])
+# 		acting_player = plays % 2
+# 		# print('*************')
+# 		# print('HISTORY RD', history[rd])
+# 		# print('PLAYS', plays)
+
+# 		if plays >= 2:
+# 			p0total = np.sum(history[rd][0::2])
+# 			p1total = np.sum(history[rd][1::2])
+# 			# print('P0 TOTAL', p0total)
+# 			# print('P1 TOTAL', p1total)
+# 			# print('ROUND BEG', rd)
+				
+# 			if p0total == p1total:
+# 				if rd == 0 and p0total != 19:
+# 					rd = 1
+# 					# print('ROUND TO 1')
+# 				else:
+# 					# print('SHOWDOWN RETURN')
+# 					winner = self.winning_hand(cards)
+# 					if winner == -1:
+# 						return 0
+# 					elif traversing_player == winner:
+# 						return pot/2
+# 					elif traversing_player != winner:
+# 						return -pot/2
+
+# 			elif history[rd][-1] == 0: #previous player folded
+# 				# print('FOLD RETURN')
+# 				if acting_player == 0 and acting_player == traversing_player:
+# 					return p1total+1
+# 				elif acting_player == 0 and acting_player != traversing_player:
+# 					return -(p1total +1)
+# 				elif acting_player == 1 and acting_player == traversing_player:
+# 					return p0total+1
+# 				elif acting_player == 1 and acting_player != traversing_player:
+# 					return -(p0total +1)
+# 		# print('ROUND AFTER', rd)
+# 		if rd == 0:
+# 			infoset = str(cards[acting_player]) + str(history)
+# 		elif rd == 1:
+# 			infoset = str(cards[acting_player]) + str(cards[2]) + str(history)
+
+# 		if acting_player == 0:
+# 			infoset_bets = self.valid_bets(history, rd, 0)
+# 		elif acting_player == 1:
+# 			infoset_bets = self.valid_bets(history, rd, 1)
+# 		# print('ROUND', rd)
+# 		# print('INFOSET BETS', infoset_bets)
+# 		if infoset not in self.nodes:
+# 			self.nodes[infoset] = Node(infoset_bets)
+
+# 		# print(self.nodes[infoset])
+# 		# print(infoset)
+
+# 		nodes_touched += 1
+
+# 		if acting_player == traversing_player:
+# 			util = defaultdict(int)
+# 			node_util = 0
+# 			strategy = self.nodes[infoset].get_strategy()
+# 			for a in infoset_bets:
+# 				if rd == 0:
+# 					next_history = [history[0] + [a], history[1]]
+# 				elif rd == 1:
+# 					next_history = [history[0], history[1] + [a]]
+# 				pot += a
+# 				util[a] = self.external_cfr(cards, next_history, rd, pot, nodes_touched, traversing_player, t)
+# 				node_util += strategy[a] * util[a]
+
+# 			for a in infoset_bets:
+# 				regret = util[a] - node_util
+# 				self.nodes[infoset].regret_sum[a] += regret
+# 			return node_util
+
+# 		else: #acting_player != traversing_player
+# 			strategy = self.nodes[infoset].get_strategy()
+# 			# print('STRATEGY', strategy)
+# 			dart = random.random()
+# 			# print('DART', dart)
+# 			strat_sum = 0
+# 			for a in strategy:
+# 				strat_sum += strategy[a]
+# 				if dart < strat_sum:
+# 					action = a
+# 					break
+# 			# print('ACTION', action)
+# 			if rd == 0:
+# 				next_history = [history[0] + [action], history[1]]
+# 			elif rd == 1:
+# 				next_history = [history[0], history[1] + [action]]
+# 			pot += action
+# 			# if acting_player == 0:
+# 			# 	p0stack -= action
+# 			# elif acting_player == 1:
+# 			# 	p1stack -= action
+# 			# print('NEXT HISTORY2', next_history)
+# 			util = self.external_cfr(cards, next_history, rd, pot, nodes_touched, traversing_player, t)
+# 			for a in infoset_bets:
+# 				self.nodes[infoset].strategy_sum[a] += strategy[a]
+# 			return util
+
+
+
 
 def main():
     global bet_choice, button_locked_until, player_is_bb, bot_should_act
